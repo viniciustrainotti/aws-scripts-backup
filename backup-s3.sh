ae1b4a05d447415758b452d93b286326af9799b2
Vinícius Trainotti <12179459+viniciustrainotti@users.noreply.github.com>
2022-12-28 22:07:58 -0300

    feat: improve script

--- backup-s3.sh
+++ backup-s3.sh
@@ -5,8 +5,6 @@ set -eou pipefail
 BUCKET="${1}"
 PROFILE="${2:-default}"
 
-mkdir -p "$BUCKET"
-cd "$BUCKET"
+mkdir -p "$BUCKET" && cd "$BUCKET"
 aws s3 sync s3://$BUCKET . --delete --profile "$PROFILE"
-cd ..
-tar -cvf "$BUCKET.tar" "$BUCKET"
+cd .. && tar -cvf "$BUCKET.tar" "$BUCKET"
