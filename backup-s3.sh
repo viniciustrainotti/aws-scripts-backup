4c3a0745990a62286d91d573984c7498992cb514
Vinícius Trainotti <12179459+viniciustrainotti@users.noreply.github.com>
2022-12-28 22:05:05 -0300

    feat: add profile to args

--- backup-s3.sh
+++ backup-s3.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# tar -cf archive.tar foo bar  # Create archive.tar from files foo and bar.
-# tar -tvf archive.tar         # List all files in archive.tar verbosely.
-# tar -xf archive.tar          # Extract all files from archive.tar.
-
 set -eou pipefail
 
 BUCKET="${1}"
+PROFILE="${2:-default}"
 
 mkdir -p "$BUCKET"
 cd "$BUCKET"
-aws s3 sync s3://$BUCKET . --delete --profile viniciustrainotti
+aws s3 sync s3://$BUCKET . --delete --profile "$PROFILE"
 cd ..
 tar -cvf "$BUCKET.tar" "$BUCKET"
